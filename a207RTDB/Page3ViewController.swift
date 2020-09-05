47104712fa9ddb3b9e03a089e23a1b276b34af5a
Danny Shen <shenfive@gmail.com>
2020-09-05 17:10:05 +0800

    send Msg

--- a207RTDB/Page3ViewController.swift
+++ a207RTDB/Page3ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class Page3ViewController: UIViewController {
     
@@ -14,13 +15,27 @@ class Page3ViewController: UIViewController {
     var key = ""
     var subject = ""
     
-
+    @IBOutlet weak var messageTF: UITextField!
+    @IBOutlet weak var page3TableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("sub:\(subject)\nkey:\(key)\nnickname:\(nickName)")
-        
+        self.title = subject
     }
     
-
+    @IBAction func submit(_ sender: Any) {
+        let ref = Database.database().reference().child("disc").child(key)
+        let message = messageTF.text ?? ""
+        if message.count <= 2 {
+            alertToUser(message: "至少輸入三個字元")
+            return
+        }
+        
+        let theMsgData:[String:Any] = ["time":ServerValue.timestamp(),"nickname":nickName,"msg":message]
+        ref.childByAutoId().setValue(theMsgData)
+        messageTF.text = ""
+    }
+    
 }
